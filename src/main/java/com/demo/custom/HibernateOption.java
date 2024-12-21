2e21345cbb34ca48d0030c293c8a9d82dd29122c
jleblanc64 <dabadich@gmail.com>
2024-12-21 12:06:10 +0100

    cleanup

--- src/main/java/com/demo/custom/HibernateOption.java
+++ src/main/java/com/demo/custom/HibernateOption.java
@@ -2,137 +2,12 @@ package com.demo.custom;
 
 import io.github.jleblanc64.libcustom.LibCustom;
 import io.vavr.control.Option;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.SneakyThrows;
-import org.hibernate.mapping.BasicValue;
-import org.hibernate.mapping.Column;
-import org.hibernate.type.descriptor.WrapperOptions;
-import org.hibernate.type.descriptor.java.JavaType;
-import org.hibernate.type.descriptor.java.spi.UnknownBasicJavaType;
 import org.hibernate.type.descriptor.jdbc.BasicBinder;
-import org.hibernate.type.descriptor.jdbc.IntegerJdbcType;
-import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
-import org.reflections.Reflections;
-
-import java.lang.reflect.Field;
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.regex.Pattern;
-
-import static io.github.jleblanc64.libcustom.functional.ListF.f;
-import static io.github.jleblanc64.libcustom.functional.OptionF.o;
 
 public class HibernateOption {
     public static Class<?> class_ = Option.class;
 
     public static void override() {
-        // replace with your own values
-        var rootPackage = "com.demo";
-
-        var tableToEntity = f(new Reflections(rootPackage).getTypesAnnotatedWith(Entity.class))
-                .toMap(x -> x.getAnnotation(Table.class).name(), x -> x);
-
-        LibCustom.override(UnknownBasicJavaType.class, "getRecommendedJdbcType", args -> {
-            var context = args[0];
-            if (!(context instanceof BasicValue))
-                return LibCustom.ORIGINAL;
-
-            var b = (BasicValue) context;
-
-            var s = b.getColumn();
-            if (!(s instanceof Column))
-                return LibCustom.ORIGINAL;
-
-            var c = (Column) s;
-
-            var entity = tableToEntity.get(b.getTable().getName());
-            var fields = f(entity.getDeclaredFields());
-            var field = fields.findSafe(x -> x.getName().equals(c.getName()));
-            if (field.getType() != class_)
-                return LibCustom.ORIGINAL;
-
-            var typeParam = typeParam(field);
-            if (typeParam == String.class)
-                return new VarcharJdbcType();
-            else if (typeParam == Integer.class)
-                return new IntegerJdbcType();
-
-            return LibCustom.ORIGINAL;
-        });
-
-        LibCustom.override(UnknownBasicJavaType.class, "unwrap", args -> {
-            var v = args[0];
-            var type = (Class<?>) args[1];
-
-            Option<?> o;
-            if (instanceOf(v, class_)) {
-
-                o = (Option<?>) v;
-                if (o.isDefined())
-                    return o.get();
-
-                return null;
-            }
-
-            if (type == class_ && !instanceOf(v, class_))
-                // return Option from nullable value v
-                return o(v);
-
-            return v;
-        });
-
-        LibCustom.overrideWithSelf(UnknownBasicJavaType.class, "wrap", argsSelf -> {
-            var args = argsSelf.args;
-            var u = (UnknownBasicJavaType) argsSelf.self;
-            var v = args[0];
-            var type = u.getJavaTypeClass();
-
-            if (instanceOf(v, class_)) {
-                var o = (Option<?>) v;
-                if (o.isDefined())
-                    return o.get();
-
-                return null;
-            }
-
-            if (type == class_ && !instanceOf(v, class_))
-                // return Option from nullable value v
-                return o(v);
-
-            return v;
-        });
-
-        // https://github.com/hibernate/hibernate-orm/blob/4fc56653a6a6de631012e9ae43f8e6a8c52ea2d7/hibernate-core/src/main/java/org/hibernate/type/descriptor/jdbc/IntegerJdbcType.java#L65-L78
-        LibCustom.overrideWithSelf(IntegerJdbcType.class, "getBinder", argSelf -> {
-            var arg = (JavaType) argSelf.args[0];
-            if (arg.getJavaTypeClass() != Option.class)
-                return LibCustom.ORIGINAL;
-
-            var javaType = (JavaType<Option<Integer>>) arg;
-            var self = (IntegerJdbcType) argSelf.self;
-
-            return new BasicBinder<>(javaType, self) {
-                @Override
-                protected void doBind(PreparedStatement st, Option<Integer> value, int index, WrapperOptions options) throws SQLException {
-                    if (value == null || value.isEmpty())
-                        st.setObject(index, null);
-                    else
-                        st.setInt(index, javaType.unwrap(value, Integer.class, options));
-                }
-
-                @Override
-                protected void doBind(CallableStatement st, Option<Integer> value, String name, WrapperOptions options)
-                        throws SQLException {
-                    if (value == null || value.isEmpty())
-                        st.setObject(name, null);
-                    else
-                        st.setInt(name, javaType.unwrap(value, Integer.class, options));
-                }
-            };
-        });
-
         LibCustom.modifyArg(BasicBinder.class, "bind", 1, args -> {
             var value = args[1];
 
@@ -143,27 +18,5 @@ public class HibernateOption {
 
             return value;
         });
-    }
-
-    static boolean instanceOf(Object o, Class<?> c) {
-        return o != null && o.getClass() == c;
-    }
-
-    @SneakyThrows
-    static Class<?> typeParam(Field field) {
-        var name = field.getGenericType().getTypeName();
-        return typeParam(name);
-    }
-
-    public static Class<?> typeParam(String name) throws ClassNotFoundException {
-        var pattern = "(?<=\\<).*(?=\\>)";
-        var subType = regex0(name, pattern);
-        return Class.forName(subType);
-    }
-
-    static String regex0(String s, String pattern) {
-        var matcher = Pattern.compile(pattern).matcher(s);
-        matcher.find();
-        return matcher.group(0);
     }
 }
