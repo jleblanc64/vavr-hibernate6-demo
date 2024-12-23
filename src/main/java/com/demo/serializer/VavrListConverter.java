7a3d7913ad40c33d575903fde1f373d2ee530527
jleblanc64 <dabadich@gmail.com>
2024-12-23 14:04:34 +0100

    cleanup

--- src/main/java/com/demo/serializer/VavrListConverter.java
+++ src/main/java/com/demo/serializer/VavrListConverter.java
@@ -1,28 +1,15 @@
 package com.demo.serializer;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.databind.util.Converter;
 import com.fasterxml.jackson.databind.util.StdConverter;
 import io.vavr.collection.List;
 
 import java.util.Collection;
 
 public class VavrListConverter {
-    public static class FromCollec implements Converter<Collection<?>, List<?>> {
+    public static class FromCollec extends StdConverter<Collection<?>, List<?>> {
         @Override
         public List<?> convert(Collection value) {
             return List.ofAll(value);
-        }
-
-        @Override
-        public JavaType getInputType(TypeFactory typeFactory) {
-            return typeFactory.constructCollectionLikeType(Collection.class, Object.class);
-        }
-
-        @Override
-        public JavaType getOutputType(TypeFactory typeFactory) {
-            return typeFactory.constructCollectionLikeType(List.class, Object.class);
         }
     }
 
